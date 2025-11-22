e68a2f0dec86ce439b3da0e1ca3e2d980f092e89
Anonymous Directive <anonymouconfidential@gmail.com>
2025-11-22 10:55:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4307152531-11b18",
+  "appId": "1:794761792460:web:676347828266cac67e2b69",
+  "apiKey": "AIzaSyCszxZZV12Z3HdYBZJUWvPq4vUqwzr6hiY",
+  "authDomain": "studio-4307152531-11b18.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "794761792460"
+};
