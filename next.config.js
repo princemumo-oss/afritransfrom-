e8b9cc1d082af7af64a698168ef2c326d741c778
Anonymous Directive <anonymouconfidential@gmail.com>
2025-11-23 15:23:33 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+        bodySizeLimit: '4mb',
+    }
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
